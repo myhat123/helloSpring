b3a9859fd907f435602eaea945a2f0fe5d5ee1cb
myhat123 <myhat123@gmail.com>
2020-04-18 18:13:32 +0800

    删除不必要的注解

--- hello_05/src/main/java/AppConfig.java
+++ hello_05/src/main/java/AppConfig.java
@@ -1,8 +1,8 @@
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import org.springframework.context.annotation.Configuration;
 
-@Configuration
+// @Configuration
 public class AppConfig {
 
     @Bean
--- hello_05/src/main/java/Store.java
+++ hello_05/src/main/java/Store.java
@@ -1,9 +1,9 @@
 
-import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.beans.factory.annotation.Autowired;
 
 public class Store {
 
-    @Autowired
+    // @Autowired
     private Item item;
   
     public Store(Item item) {
